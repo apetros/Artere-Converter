220f350a90cdf2bf8eeec1c9ca71f27adffce82b
Nontas Papadopoulos <59374756+NontasPapapadopoulos@users.noreply.github.com>
2021-01-25 01:35:58 +0200

    changes on variable names

--- from_artere.py
+++ from_artere.py
@@ -3,24 +3,19 @@ The aim of this program is to convert a .dat file which is used for the ARTERE p
 This program reads and grabs the data from the .dat file and creates a Python file in order
 to make a network with pandapower.
 """
-
 import math
-
-
-bus_list = []
-bus_names_list = []
+busList = []
 init_vm_pu_list = []
 init_va_degree_list = []
 load_list = []
-gen_list = []
-slack_bus_list = []
-transformer_list = []
-line_list = []
-switch_list = []
-shunt_list = []
-Bus_dict = {}
+generatorList = []
+transformersList = []
+linesList = []
+switchList = []
+shuntsList = []
+busDict = {}
 bus_voltage_dict = {}
-gen_dict = {}
+genDict = {}
 
 
 def check_for_int_bus(bus_name):
@@ -52,8 +47,7 @@ def from_artere(artereFile):
             line = Line.split()
             if len(line) > 0:
                 if line[0] == 'SLACK':
-                    slack_bus_name = line[1]
-                    slack_bus_list.append(slack_bus_name)
+                    slack_bus = line[1]
                 global frequency
                 if line[0] == 'FNOM':
                     frequency = float(line[1])
@@ -89,16 +83,15 @@ def from_artere(artereFile):
                         bus_name = bus_name.replace(" ", "_")
                     except:
                         pass
-                    bus_names_list.append(bus_name)
 
                     # Create a bus
-                    Bus_dict[bus_name] = {}
-                    Bus_dict[bus_name]["name"] = bus_name
-                    Bus_dict[bus_name]["voltage"] = vn_kv
+                    busDict[bus_name] = {}
+                    busDict[bus_name]["name"] = bus_name
+                    busDict[bus_name]["voltage"] = vn_kv
 
                     # Create a string that is going to be stored in the Python file.
                     created_bus = f"{bus_name} = pp.create_bus(net, vn_kv={vn_kv}, name=\"{bus_name}\", in_service=True)\n"
-                    bus_list.append(created_bus)
+                    busList.append(created_bus)
 
                     # If the bus has a load, create a load connected to the bus.
                     if Pload > 0 or Qload > 0:
@@ -112,7 +105,7 @@ def from_artere(artereFile):
                         load_list.append(created_load)
                     if Bshunt != 0:
                         created_shunt = f"pp.create_shunt(net, bus={bus_name}, q_mvar={Bshunt}, in_service=True)\n"
-                        shunt_list.append(created_shunt)
+                        shuntsList.append(created_shunt)
 
                 # Transmittion lines
                 if line[0] == 'LINE':
@@ -131,7 +124,7 @@ def from_artere(artereFile):
                     to_bus = check_for_int_bus(to_bus)
 
                     # Calculate the maximum curent
-                    line_voltage = Bus_dict[from_bus]["voltage"]
+                    line_voltage = busDict[from_bus]["voltage"]
                     Imax = (Snom * 10 ** 6) / (math.sqrt(3) * line_voltage * 10 ** 3)
                     Imax = Imax / 1000
                     Imax = format(Imax, '.6f')
@@ -141,9 +134,9 @@ def from_artere(artereFile):
                     Capacitance = Capacitance * 10 ** 9     # nF
                     Capacitance = format(Capacitance, '.5f')
 
-                    created_line = f'pp.create_line_from_parameters(net, from_bus={Bus_dict[from_bus]["name"]}, to_bus={Bus_dict[to_bus]["name"]}, name="{line_name}"' \
+                    created_line = f'pp.create_line_from_parameters(net, from_bus={busDict[from_bus]["name"]}, to_bus={busDict[to_bus]["name"]}, name="{line_name}"' \
                                    f', length_km=1, r_ohm_per_km={R_Ohm}, x_ohm_per_km={X_Ohm}, max_i_ka={Imax}, c_nf_per_km={Capacitance}, in_service={in_service})\n'
-                    line_list.append(created_line)
+                    linesList.append(created_line)
 
                 # LTC-V
                 if line[0] == 'LTC-V':
@@ -170,16 +163,16 @@ def from_artere(artereFile):
                     in_service = checkStatus(On_off)
 
                     # vk, vkr, i0_percent
-                    Ym = Susceptance
+                    ym = Susceptance
                     rk = Resistance
                     xk = Xreactance
                     zk = math.sqrt(pow(rk, 2) + pow(xk, 2))
                     vk_percent = zk
                     vkr_percent = rk
-                    i0_percent = Ym
+                    i0_percent = ym
 
                     # Check which bus is High level and which is low.
-                    if Bus_dict[from_bus]['voltage'] > Bus_dict[to_bus]['voltage']:
+                    if busDict[from_bus]['voltage'] > busDict[to_bus]['voltage']:
                         high_lvl_bus = from_bus
                         low_lvl_bus = to_bus
                     else:
@@ -205,14 +198,14 @@ def from_artere(artereFile):
                     # Create a transformer
                     # if the ratio is not 100, we should take into account the taps.
                     if N_ratio == 100:
-                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={Bus_dict[high_lvl_bus]["name"]}, lv_bus={Bus_dict[low_lvl_bus]["name"]}, sn_mva={Snom}, ' \
-                                              f'name={transfo_name}, shift_degree={PHI}, vn_hv_kv={Bus_dict[high_lvl_bus]["voltage"]}, vn_lv_kv={Bus_dict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent}, pfe_kw=0, i0_percent={i0_percent}, in_service={in_service})\n'
+                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={busDict[high_lvl_bus]["name"]}, lv_bus={busDict[low_lvl_bus]["name"]}, sn_mva={Snom}, ' \
+                                              f'name={transfo_name}, shift_degree={PHI}, vn_hv_kv={busDict[high_lvl_bus]["voltage"]}, vn_lv_kv={busDict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent}, pfe_kw=0, i0_percent={i0_percent}, in_service={in_service})\n'
                     else:
-                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={Bus_dict[high_lvl_bus]["name"]}, lv_bus={Bus_dict[low_lvl_bus]["name"]}, sn_mva={Snom}, name={transfo_name}, ' \
-                                              f'shift_degree={PHI}, vn_hv_kv={Bus_dict[high_lvl_bus]["voltage"]}, vn_lv_kv={Bus_dict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent},' \
+                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={busDict[high_lvl_bus]["name"]}, lv_bus={busDict[low_lvl_bus]["name"]}, sn_mva={Snom}, name={transfo_name}, ' \
+                                              f'shift_degree={PHI}, vn_hv_kv={busDict[high_lvl_bus]["voltage"]}, vn_lv_kv={busDict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent},' \
                                               f' pfe_kw=0, i0_percent={i0_percent}, tap_min={tap_min}, tap_max={tap_max}, tap_step_percent={tap_step_percent}, tap_pos={tap_pos},tap_neutral={tap_neutral}, tap_side={tap_side}, in_service={in_service})\n'
 
-                    transformer_list.append(created_transformer)
+                    transformersList.append(created_transformer)
 
                 # Transformer for TRFO record
                 if line[0] == 'TRFO':
@@ -263,7 +256,7 @@ def from_artere(artereFile):
                     i0_percent = Ym
 
                     # Check which bus is High level and which is low.
-                    if Bus_dict[from_bus]["voltage"] > Bus_dict[to_bus]["voltage"]:
+                    if busDict[from_bus]["voltage"] > busDict[to_bus]["voltage"]:
                         high_lvl_bus = from_bus
                         low_lvl_bus = to_bus
                     else:
@@ -279,9 +272,9 @@ def from_artere(artereFile):
                         pass
 
                     try:
-                        if Bus_dict[CON_BUS]['voltage'] == Bus_dict[high_lvl_bus]['voltage']:
+                        if busDict[CON_BUS]['voltage'] == busDict[high_lvl_bus]['voltage']:
                             tap_side = "\"hv\""
-                        elif Bus_dict[CON_BUS]['voltage'] == Bus_dict[low_lvl_bus]['voltage']:
+                        elif busDict[CON_BUS]['voltage'] == busDict[low_lvl_bus]['voltage']:
                             tap_side = "\"lv\""
                         else:
                             tap_side = None
@@ -308,11 +301,11 @@ def from_artere(artereFile):
                             tap_side = "\"lv\""
 
                         # Create a string
-                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={Bus_dict[high_lvl_bus]["name"]}, lv_bus={Bus_dict[low_lvl_bus]["name"]}, sn_mva={Snom}, name="{transfo_name}", ' \
-                                              f'vn_hv_kv={Bus_dict[high_lvl_bus]["voltage"]}, vn_lv_kv={Bus_dict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent}, pfe_kw=0, i0_percent={i0_percent}, ' \
+                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={busDict[high_lvl_bus]["name"]}, lv_bus={busDict[low_lvl_bus]["name"]}, sn_mva={Snom}, name="{transfo_name}", ' \
+                                              f'vn_hv_kv={busDict[high_lvl_bus]["voltage"]}, vn_lv_kv={busDict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent}, pfe_kw=0, i0_percent={i0_percent}, ' \
                                               f'tap_pos={tap_pos}, tap_neutral={tap_neutral}, tap_step_percent={tap_step_percent}, tap_side={tap_side}, tap_min={tap_min}, tap_max={tap_max}, in_service={in_service})\n'
 
-                        transformer_list.append(created_transformer)
+                        transformersList.append(created_transformer)
 
                     # if N_ratio , N_First, N_last are equal:
                     elif N_ratio != 100 and N_First == N_Last and N_Last == N_ratio:
@@ -333,21 +326,21 @@ def from_artere(artereFile):
                             #tap_side = "\"lv\""
 
                         # Create a string
-                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={Bus_dict[high_lvl_bus]["name"]}, lv_bus={Bus_dict[low_lvl_bus]["name"]}, sn_mva={Snom}, name="{transfo_name}", ' \
-                                                  f'vn_hv_kv={Bus_dict[high_lvl_bus]["voltage"]}, vn_lv_kv={Bus_dict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent}, pfe_kw=0, i0_percent={i0_percent}, ' \
+                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={busDict[high_lvl_bus]["name"]}, lv_bus={busDict[low_lvl_bus]["name"]}, sn_mva={Snom}, name="{transfo_name}", ' \
+                                                  f'vn_hv_kv={busDict[high_lvl_bus]["voltage"]}, vn_lv_kv={busDict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent}, pfe_kw=0, i0_percent={i0_percent}, ' \
                                                   f'tap_pos={tap_pos}, tap_neutral={tap_neutral}, tap_step_percent={tap_step_percent}, tap_side={tap_side}, tap_min={tap_min}, tap_max={tap_max}, in_service={in_service})\n'
 
                         # Store the string to a list
-                        transformer_list.append(created_transformer)
+                        transformersList.append(created_transformer)
 
                     elif N_ratio == 100 and N_First == 0 and N_Last == 0:
                         # Create a string
-                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={Bus_dict[high_lvl_bus]["name"]}, lv_bus={Bus_dict[low_lvl_bus]["name"]}, sn_mva={Snom}, name="{transfo_name}", ' \
-                                              f'vn_hv_kv={Bus_dict[high_lvl_bus]["voltage"]}, vn_lv_kv={Bus_dict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent}, pfe_kw=0, ' \
+                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={busDict[high_lvl_bus]["name"]}, lv_bus={busDict[low_lvl_bus]["name"]}, sn_mva={Snom}, name="{transfo_name}", ' \
+                                              f'vn_hv_kv={busDict[high_lvl_bus]["voltage"]}, vn_lv_kv={busDict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent}, pfe_kw=0, ' \
                                               f'i0_percent={i0_percent}, in_service={in_service})\n'
 
                         # Store the string to a list
-                        transformer_list.append(created_transformer)
+                        transformersList.append(created_transformer)
 
                     elif N_ratio > 0 and N_First > 0 and N_Last > 0 and NBPOS > 0:
 
@@ -359,11 +352,11 @@ def from_artere(artereFile):
                         tap_pos = (float(N_ratio) - N_First) / tap_step_percent
                         tap_pos = round(tap_pos)
 
-                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={Bus_dict[high_lvl_bus]["name"]}, lv_bus={Bus_dict[low_lvl_bus]["name"]}, sn_mva={Snom}, name="{transfo_name}", ' \
-                                                  f'vn_hv_kv={Bus_dict[high_lvl_bus]["voltage"]}, vn_lv_kv={Bus_dict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent}, pfe_kw=0, i0_percent={i0_percent}, ' \
+                        created_transformer = f'pp.create_transformer_from_parameters(net, hv_bus={busDict[high_lvl_bus]["name"]}, lv_bus={busDict[low_lvl_bus]["name"]}, sn_mva={Snom}, name="{transfo_name}", ' \
+                                                  f'vn_hv_kv={busDict[high_lvl_bus]["voltage"]}, vn_lv_kv={busDict[low_lvl_bus]["voltage"]}, vkr_percent={vkr_percent}, vk_percent={vk_percent}, pfe_kw=0, i0_percent={i0_percent}, ' \
                                                   f'tap_pos={tap_pos}, tap_neutral={tap_neutral}, tap_step_percent={tap_step_percent}, tap_side={tap_side}, tap_min=0, tap_max={NBPOS-1}, in_service={in_service})\n'
 
-                        transformer_list.append(created_transformer)
+                        transformersList.append(created_transformer)
 
                 # Generators
                 if line[0] == 'GENER':
@@ -383,21 +376,21 @@ def from_artere(artereFile):
                     # if VIMP is zero, the generator is treated as a PQ bus (we create sgen)
                     # Otherwise it is treated as a PV bus and the Q is ignored
                     if VIMP == "0":
-                        created_sgen = f'pp.create_sgen(net, bus={Bus_dict[bus_connected]["name"]}, p_mw={Pproduction}, q_mvar={Qproduction}, sn_mva={Snom}, name="{gener_name}", max_q_mvar={Qmax}, min_q_mvar={Qmin}, in_service={in_service})\n'
-                        gen_list.append(created_sgen)
+                        created_sgen = f'pp.create_sgen(net, bus={busDict[bus_connected]["name"]}, p_mw={Pproduction}, q_mvar={Qproduction}, sn_mva={Snom}, name="{gener_name}", max_q_mvar={Qmax}, min_q_mvar={Qmin}, in_service={in_service})\n'
+                        generatorList.append(created_sgen)
 
                     else:
-                        if gener_name in slack_bus_list:
-                            created_gen = f'pp.create_gen(net, p_mw={Pproduction}, max_q_mvar={Qmax}, min_q_mvar={Qmin}, sn_mva={Snom}, bus={Bus_dict[bus_connected]["name"]}, vm_pu={VIMP}, name="{gener_name}", slack={True}, in_service={in_service})\n'
-                            gen_list.append(created_gen)
+                        if gener_name == slack_bus:
+                            created_gen = f'pp.create_gen(net, p_mw={Pproduction}, max_q_mvar={Qmax}, min_q_mvar={Qmin}, sn_mva={Snom}, bus={busDict[bus_connected]["name"]}, vm_pu={VIMP}, name="{gener_name}", slack={True}, in_service={in_service})\n'
+                            generatorList.append(created_gen)
 
                         elif gener_name == "Ext_Grid":
-                            created_gen = f'pp.create_gen(net, p_mw={Pproduction}, max_q_mvar={Qmax}, min_q_mvar={Qmin}, sn_mva={Snom}, bus={Bus_dict[bus_connected]["name"]}, vm_pu={VIMP}, name="{gener_name}", slack={True}, in_service={in_service})\n'
-                            gen_list.append(created_gen)
+                            created_gen = f'pp.create_gen(net, p_mw={Pproduction}, max_q_mvar={Qmax}, min_q_mvar={Qmin}, sn_mva={Snom}, bus={busDict[bus_connected]["name"]}, vm_pu={VIMP}, name="{gener_name}", slack={True}, in_service={in_service})\n'
+                            generatorList.append(created_gen)
 
                         else:
-                            created_gen = f'pp.create_gen(net, p_mw={Pproduction}, max_q_mvar={Qmax}, min_q_mvar={Qmin}, sn_mva={Snom}, bus={Bus_dict[bus_connected]["name"]}, vm_pu={VIMP}, name="{gener_name}", slack={False}, in_service={in_service})\n'
-                            gen_list.append(created_gen)
+                            created_gen = f'pp.create_gen(net, p_mw={Pproduction}, max_q_mvar={Qmax}, min_q_mvar={Qmin}, sn_mva={Snom}, bus={busDict[bus_connected]["name"]}, vm_pu={VIMP}, name="{gener_name}", slack={False}, in_service={in_service})\n'
+                            generatorList.append(created_gen)
 
                 # Switches
                 if line[0] == 'SWITCH':
@@ -408,8 +401,8 @@ def from_artere(artereFile):
 
                     in_service = checkStatus(On_off)
 
-                    created_switch = f'pp.create_switch(net, bus={Bus_dict[first_bus]["name"]}, name="{switch_name}" ,element={second_bus}, et="b" ,closed={in_service})\n'
-                    switch_list.append(created_switch)
+                    created_switch = f'pp.create_switch(net, bus={busDict[first_bus]["name"]}, name="{switch_name}" ,element={second_bus}, et="b" ,closed={in_service})\n'
+                    switchList.append(created_switch)
 
                 # LFRESV specifies the voltage magnitude and phase at a bus.
                 if line[0] == 'LFRESV':
@@ -436,7 +429,7 @@ net = pp.create_empty_network(f_hz={frequency}, sn_mva={100})
     
     """)
         pythonFile.write('\n# list of Buses:\n')
-        for bus in bus_list:
+        for bus in busList:
             pythonFile.write(bus)
 
         pythonFile.write('\n\n# list of Loads:\n')
@@ -444,23 +437,23 @@ net = pp.create_empty_network(f_hz={frequency}, sn_mva={100})
             pythonFile.write(load)
 
         pythonFile.write('\n\n# List of Shunts:\n')
-        for shunt in shunt_list:
+        for shunt in shuntsList:
             pythonFile.write(shunt)
 
         pythonFile.write('\n\n# list of Lines:\n')
-        for line in line_list:
+        for line in linesList:
             pythonFile.write(line)
 
         pythonFile.write('\n\n# list of Transformers:\n')
-        for transformer in transformer_list:
+        for transformer in transformersList:
             pythonFile.write(transformer)
 
         pythonFile.write('\n\n# list of Generators:\n')
-        for generator in gen_list:
+        for generator in generatorList:
             pythonFile.write(generator)
 
         pythonFile.write('\n\n# List of Switches:\n')
-        for switch in switch_list:
+        for switch in switchList:
             pythonFile.write(switch)
 
         pythonFile.write(f"""
